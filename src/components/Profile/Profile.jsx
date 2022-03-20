06662ebd1b7705adad9603f790fe3f01134b0411
Andrii <Andrii@Admins-MacBook-Pro.local>
2022-03-20 18:06:44 +0200

    1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import styles from './Profile.module.css';
+
+export const Profile = ({user: {username, tag, location, avatar, stats}}) => {
+  return (
+    <div className={styles.Profile}>       
+      <div className={styles.Description}>
+        <img
+          src={avatar}
+          alt={username}
+          className={styles.Avatar}
+        />
+        <p className={styles.Name}>{username}</p>
+        <p className={styles.Tag}>@{tag}</p>
+        <p className={styles.Location}>{location}</p>
+      </div>
+
+      <ul className={styles.Stats}>
+        <li>
+          <span className={styles.Label}>Followers</span>
+          <span className={styles.Quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={styles.Label}>Views</span>
+          <span className={styles.Quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={styles.Label}>Likes</span>
+          <span className={styles.Quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>    
+  );  
+};
+    
